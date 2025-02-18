6573bd8179cda8b4b0b7a0374d103fa7269de43e
Nili Scharf <nilischarf@gmail.com>
2025-02-18 15:47:03 +0200

    complete sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1] 
+    
+    if length == 0: 
+        print([])
+        return 
+    if length == 1:
+        print([0])
+        return 
+    if length == 2:
+        print([0, 1])
+        return 
+    
+    for _ in range(2, length):
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci[:length])
+        
